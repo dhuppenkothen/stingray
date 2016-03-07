4a64d1d9ce3122bc98ff859cb0b2d10fc776b6b5
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2016-03-07 11:24:10 +0100

    update tests to account for uncertainties

--- stingray/tests/test_utils.py
+++ stingray/tests/test_utils.py
@@ -13,40 +13,49 @@ class TestRebinData(object):
         cls.counts = 2.0
         cls.x = np.arange(cls.dx/2, cls.dx/2+cls.n*cls.dx, cls.dx)
         cls.y = np.zeros_like(cls.x)+cls.counts
+        cls.yerr = np.zeros_like(cls.x)+np.sqrt(cls.counts)
 
     def test_new_stepsize(self):
         dx_new = 2.0
-        xbin, ybin, step_size = utils.rebin_data(self.x, self.y, dx_new)
+        xbin, ybin, yerrbin, step_size = utils.rebin_data(self.x, self.y,
+                                                          self.yerr, dx_new)
         assert step_size == dx_new/self.dx
 
     def test_arrays(self):
         dx_new = 2.0
-        xbin, ybin, step_size = utils.rebin_data(self.x, self.y, dx_new)
+        xbin, ybin, yerrbin, step_size = utils.rebin_data(self.x, self.y,
+                                                          self.yerr, dx_new)
         assert isinstance(xbin, np.ndarray)
         assert isinstance(ybin, np.ndarray)
+        assert isinstance(yerrbin, np.ndarray)
 
     def test_length_matches(self):
         dx_new = 2.0
-        xbin, ybin, step_size = utils.rebin_data(self.x, self.y, dx_new)
+        xbin, ybin, yerrbin, step_size = utils.rebin_data(self.x, self.y,
+                                                          self.yerr, dx_new)
         assert xbin.shape[0] == ybin.shape[0]
+        assert ybin.shape[0] == yerrbin.shape[0]
 
 
     def test_binned_counts(self):
         dx_new = 2.0
 
-        xbin, ybin, step_size = utils.rebin_data(self.x, self.y, dx_new)
+        xbin, ybin, yerrbin, step_size = utils.rebin_data(self.x, self.y,
+                                                          self.yerr, dx_new)
 
         ybin_test = np.zeros_like(xbin) + self.counts*dx_new/self.dx
         assert np.allclose(ybin, ybin_test)
 
     def test_uneven_bins(self):
         dx_new = 1.5
-        xbin, ybin, step_size = utils.rebin_data(self.x, self.y, dx_new)
+        xbin, ybin, yerrbin, step_size = utils.rebin_data(self.x, self.y,
+                                                          self.yerr, dx_new)
         assert np.isclose(xbin[1]-xbin[0], dx_new)
 
     def test_uneven_binned_counts(self):
         dx_new = 1.5
-        xbin, ybin, step_size = utils.rebin_data(self.x, self.y, dx_new)
+        xbin, ybin, yerrbin, step_size = utils.rebin_data(self.x, self.y,
+                                                          self.yerr, dx_new)
         print(xbin)
         print(ybin)
         ybin_test = np.zeros_like(xbin) + self.counts*dx_new/self.dx
