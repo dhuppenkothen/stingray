6f11f9bf4e327cc3c5c23ecd38978eefd4bea205
Abigail Stevens <abigailstev@gmail.com>
2018-05-10 17:12:09 -0400

    Fixed some pep8
    Fixed some pep8 stuff caught by pep8 speaks.

--- stingray/powerspectrum.py
+++ stingray/powerspectrum.py
@@ -146,7 +146,8 @@ class Powerspectrum(Crossspectrum):
             The normalization of the periodogram.
 
         freq: numpy.ndarray
-            The array of mid-bin frequencies in Hz that the Fourier transform samples.
+            The array of mid-bin frequencies in Hz that the Fourier transform
+            samples.
 
         power: numpy.ndarray
             The array of normalized squared absolute values of Fourier
@@ -499,12 +500,13 @@ class DynamicalPowerspectrum(AveragedPowerspectrum):
 
     def _make_matrix(self, lc):
         """Create the matrix with freq and time columns."""
-        ps_all, _ = AveragedPowerspectrum._make_segment_spectrum(
-            self, lc, self.segment_size)
+        ps_all, _ = AveragedPowerspectrum._make_segment_spectrum(self, lc,
+                        self.segment_size)
         self.dyn_ps = np.array([ps.power for ps in ps_all]).T
 
         self.freq = ps_all[0].freq
-        self.time = np.arange(lc.time[0] - 0.5 * lc.dt + 0.5 * self.segment_size,
+        self.time = np.arange(lc.time[0] - 0.5 * lc.dt + 0.5 *
+                              self.segment_size,
                               lc.time[-1] + 0.5 * lc.dt, self.segment_size)
 
         # Assign lenght of lightcurve as time resolution if only one value
